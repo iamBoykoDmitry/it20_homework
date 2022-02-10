7c22af9b616aa3f99545ce14b41d7f8fb0af4e67
Dmitry <92401949+iamBoykoDmitry@users.noreply.github.com>
2022-02-10 14:56:54 +0300

    Add files via upload

---
+++ src/mysql/hw_1/education_second.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 10 2022 г., 14:53
+-- Версия сервера: 8.0.24
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `education`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `news`
+--
+
+CREATE TABLE `news` (
+  `id` int NOT NULL,
+  `news_title` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `news_text` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `image` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `author` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `publication_date` date NOT NULL,
+  `news_activity` varchar(50) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `regions`
+--
+
+CREATE TABLE `regions` (
+  `id` int NOT NULL,
+  `region` varchar(50) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name_patronymic_surname` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `adress` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `birth_date` date NOT NULL,
+  `role` varchar(50) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name_patronymic_surname`, `adress`, `birth_date`, `role`) VALUES
+(1, 'Ivan Abramov', 'Akademichna St, 56, Zaporizhzhia', '1986-09-21', 'client'),
+(2, 'Evgeniy Chebatkov', 'Orlovs\'ka St, 24, Kharkiv', '1990-09-13', 'client');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `regions`
+--
+ALTER TABLE `regions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
